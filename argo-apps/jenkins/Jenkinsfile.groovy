8597b277d01d39896f7857813184baa2a490cd3f
tbalza <tomas.balza@gmail.com>
2024-05-31 17:41:41 +0200

    jenkins. kaniko/agents setup

--- argo-apps/jenkins/Jenkinsfile.groovy
+++ argo-apps/jenkins/Jenkinsfile.groovy
@@ -6,8 +6,8 @@ pipeline {
         }
     }
     environment {
-        AWS_DEFAULT_REGION = 'your-aws-region' // Specify the AWS region
-        IMAGE_NAME = "${env.ECR_URL}/my-django-app" // Ensure the ECR URL is correctly defined
+        AWS_DEFAULT_REGION = 'us-east-1' // Specify the AWS region
+        IMAGE_NAME = "350206045032.dkr.ecr.us-east-1.amazonaws.com/django-production" //"${env.ECR_URL}" // Ensure the ECR URL is correctly defined
     }
 
     stages {
