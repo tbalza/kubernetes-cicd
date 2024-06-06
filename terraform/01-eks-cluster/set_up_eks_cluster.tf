2526353802459142e78cc9447fc3321018084b10
tbalza <tomas.balza@gmail.com>
2024-06-06 15:45:05 +0200

    ACM and externalDNS with Cloudflare. working config for argo

--- terraform/01-eks-cluster/set_up_eks_cluster.tf
+++ terraform/01-eks-cluster/set_up_eks_cluster.tf
@@ -260,7 +260,7 @@ module "eks" {
       }
 
       #force_update_version = true
-      instance_types = ["t3.large"] # Overrides default instance defined above
+      instance_types = ["t3.medium"] # Overrides default instance defined above
 
       description = "CI-CD managed node group launch template"
 
