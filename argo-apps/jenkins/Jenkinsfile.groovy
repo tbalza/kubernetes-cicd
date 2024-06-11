cf1d29095729cfd780b0c0cf391f95183a79f051
tbalza <tomas.balza@gmail.com>
2024-06-11 12:21:37 +0200

    Jenkins. kaniko setup

--- argo-apps/jenkins/Jenkinsfile.groovy
+++ argo-apps/jenkins/Jenkinsfile.groovy
@@ -1,12 +1,30 @@
 pipeline {
     agent {
         kubernetes {
-            inheritFrom 'default'
+            yaml '''
+            apiVersion: v1
+            kind: Pod
+            metadata:
+                name: kaniko
+            spec:
+              containers:
+              - name: kaniko
+                image: gcr.io/kaniko-project/executor:debug:v1.23.1-debug
+                ttyEnabled: true
+                volumeMounts:
+                - name: django-app
+                  mountPath: "/django"
+              restartPolicy: Never
+              volumes:
+              - name: django-app
+                persistentVolumeClaim:
+                  claimName: "django-pvc"
+            '''
         }
     }
     environment {
         AWS_DEFAULT_REGION = 'us-east-1' // Specify the AWS region
-        ECR_URL = "350206045032.dkr.ecr.us-east-1.amazonaws.com/django-production" //"${env.ECR_URL}"
+        ECR_URL = "350206045032.dkr.ecr.us-east-1.amazonaws.com/django-production" // Use the direct ECR URL
     }
 
     stages {
