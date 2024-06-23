0e31220fe67f90772d128cc11559ab93f133522d
tbalza <tomas.balza@gmail.com>
2024-06-23 14:34:14 +0200

    Django. deployment with argocd

--- django/myproject/settings.py
+++ django/myproject/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -118,6 +114,9 @@ USE_I18N = True
 USE_TZ = True
 
 
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
@@ -125,7 +124,7 @@ USE_TZ = True
 # Ensure there's a leading and trailing slash for URL use in STATIC_URL (this is for https://domain/static/ )
 STATIC_URL = '/static/'
 # BASE_DIR is the root of the django project, and STATIC_ROOT is where static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/static'  # os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
