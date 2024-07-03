a2d94100b2b6170f71b9e1d1e1ab42b5f7232362
tbalza <tomas.balza@gmail.com>
2024-07-03 19:17:04 +0200

    jenkins. pipeline only triggers image build when django has changed. testing change in django folder

--- django/myproject/urls.py
+++ django/myproject/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the root.")
+    return HttpResponse("Hello, world. Sample Change.")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
