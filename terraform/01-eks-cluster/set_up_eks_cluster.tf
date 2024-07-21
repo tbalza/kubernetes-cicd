a6ed28a7e7d92b16b56abf4ac8ddb5069292d82a
tbalza <tomas.balza@gmail.com>
2024-07-21 20:29:57 +0200

    refactor. using argocd cmp to allow kustomize to dynamically substitute aws acct # from terraform data (and not have it hardcoded in the repo)

--- terraform/01-eks-cluster/set_up_eks_cluster.tf
+++ terraform/01-eks-cluster/set_up_eks_cluster.tf
@@ -618,7 +618,21 @@ module "eks" {
       }
     }
 
-    imageupdater = {
+    argocdrepo = {
+      principal_arn     = aws_iam_role.argo_cd_repo.arn
+      kubernetes_groups = []
+
+      policy_associations = {
+        admin_policy = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy" # check
+          access_scope = {
+            type = "cluster" # check
+          }
+        }
+      }
+    }
+
+    imageupdater = { # argocd image updater
       principal_arn     = aws_iam_role.image_updater.arn
       kubernetes_groups = []
 
@@ -702,6 +716,36 @@ module "eks" {
 
 ## STS
 
+resource "aws_iam_role" "argo_cd_repo" {
+  name = "ArgoCDrepoRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "eks.amazonaws.com"
+        },
+      },
+      # External Secrets Operator reqs (jwt auth)
+      {
+        Effect = "Allow",
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Principal = {
+          Federated = module.eks.oidc_provider_arn
+        },
+        Condition = {
+          StringEquals = {
+            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub" : "system:serviceaccount:argocd:argocd-repo-server" #"namespace:service-account-name"
+          }
+        }
+      },
+    ]
+  })
+}
+
 resource "aws_iam_role" "argo_cd" {
   name = "ArgoCDRole"
 
@@ -724,7 +768,7 @@ resource "aws_iam_role" "argo_cd" {
         },
         Condition = {
           StringEquals = {
-            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub" : "system:serviceaccount:argocd:argocd-repo-server" #"namespace:service-account-name"
+            "${replace(module.eks.cluster_oidc_issuer_url, "https://", "")}:sub" : "system:serviceaccount:argocd:*" #"namespace:service-account-name"
           }
         }
       },
